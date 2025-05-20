b1f33c745d5790565e1bac3cb7cba45766b3999f
MARYAMTEE <ayorindemaryam078@gmail.com>
2025-05-20 16:37:06 +0100

    update cancel icon

--- script.js
+++ script.js
@@ -1,6 +1,11 @@
 const openMenuBtn = document.getElementById("toggleSidebar");
 const sidebar = document.getElementById("nav");
+const closeMenuBtn = document.querySelector("#cancelSidebar")
 
+// Toggle visibility
 openMenuBtn.addEventListener("click", () => {
     sidebar.classList.toggle("hidden");
 });
+
+//Close sidebar
+closeMenuBtn.addEventListener("click", () => openMenuBtn.click());
