6c6577e005515b517b0c251b89483d1a6575ea8c
MARYAMTEE <ayorindemaryam078@gmail.com>
2025-05-20 16:54:17 +0100

    update sidebar

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const openMenuBtn = document.getElementById("toggleSidebar");
 const sidebar = document.getElementById("nav");
-const closeMenuBtn = document.querySelector("#cancelSidebar")
+const closeMenuBtn = document.querySelector("#cancelSidebar");
+const closeSideBar = document.querySelectorAll(".menu .menu-item");
 
 // Toggle visibility
 openMenuBtn.addEventListener("click", () => {
@@ -9,3 +10,8 @@ openMenuBtn.addEventListener("click", () => {
 
 //Close sidebar
 closeMenuBtn.addEventListener("click", () => openMenuBtn.click());
+
+//Close side bar when any nav item is clicked
+closeSideBar.forEach(link => {
+	link.addEventListener("click", () => openMenuBtn.click())
+});
